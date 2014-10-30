6af1161676257b525e1f86a356a91a2f3ad1a679
Andre Pacheco <apacheco@mymail.mines.edu>
2014-10-29 21:03:48 -0600

    Changed variable names in HumanPlayer to be more intuitive.

--- clueGame/HumanPlayer.java
+++ clueGame/HumanPlayer.java
@@ -3,8 +3,8 @@ package clueGame;
 import java.awt.Color;
 
 public class HumanPlayer extends Player {
-	public HumanPlayer(String name, Color color, int yCoord, int xCoord) {
-		super(name, color, yCoord, xCoord);
+	public HumanPlayer(String name, Color color, int col, int row) {
+		super(name, color, col, row);
 
 	}
 }
